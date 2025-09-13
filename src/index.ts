66be2f042f4e7060a35c0537fd2829240be0c5d8
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2025-09-13 05:45:15 +0530

    Practising basic CRUD query for postgres

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { Client } from 'pg';
+import "dotenv/config";
+
+async function insertData () {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+
+  try {
+    await client.connect();
+    const insertQuery = `INSERT INTO users (username, email) VALUES ('gemini', 'gemini@google.com');`;
+    await client.query(insertQuery);
+    console.log("Insertion success");
+  } catch (e) {
+    console.error("Error while inserting",e);
+  } finally {
+    await client.end();
+  }
+}
+
+insertData();
